8b524090f067249ccf4591bf58d0e16f256858da
Vineeth V Nair <vineethvnair0@gmail.com>
2018-03-30 21:29:56 +0530

    merge sort different approach

---
+++ sorting/src/main/java/MergeSortWithOneArray.java
@@ -0,0 +1,50 @@
+public class MergeSortWithOneArray {
+
+    int[] helper;
+
+    public int[] sort(int[] arr) {
+        if(arr == null)
+            throw new NullPointerException("Input cannot be null");
+        helper = new int[arr.length];
+        mergeSort(arr,0,arr.length-1);
+        return arr;
+    }
+
+    public void mergeSort(int[] arr,int low,int high){
+        if(low < high){
+            int mid = (low+high)/2;
+            mergeSort(arr,low,mid);
+            mergeSort(arr,mid+1,high);
+            merge(arr,low,mid,high);
+        }
+    }
+
+    public void merge(int[] arr,int low,int mid,int high){
+
+      for (int i=low;i<=high;i++)
+          helper[i] = arr[i];
+
+      int i = low;
+      int j = mid+1;
+      int k = low;
+
+      while ((i<=mid) && (j<=high)){
+          if(helper[i] <= helper[j]){
+              arr[k] = helper[i];
+              i++;
+          } else {
+              arr[k] = helper[j];
+              j++;
+          }
+          k++;
+      }
+
+      while (i<=mid){
+          arr[k] = helper[i];
+          i++;
+          k++;
+      }
+
+    }
+
+}
---
+++ sorting/src/test/java/MergeSortWithOneArrayTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MergeSortWithOneArrayTest {
+
+    MergeSortWithOneArray mergeSortWithOneArray;
+
+    @Before
+    public void setup(){
+        mergeSortWithOneArray = new MergeSortWithOneArray();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSortForNull(){
+        mergeSortWithOneArray.sort(null);
+    }
+
+    @Test
+    public void testSortForEmpty(){
+        int[] a = {};
+        int[] result = mergeSortWithOneArray.sort(a);
+        assertArrayEquals(a,result);
+    }
+
+    @Test
+    public void testSortForOdd(){
+        int[] a = {2,4,1,6,8,5,3,7};
+        int[] expected = {1,2,3,4,5,6,7,8};
+        int[] result = mergeSortWithOneArray.sort(a);
+        assertArrayEquals(expected,result);
+    }
+    @Test
+    public void testMergeSortEven(){
+        int[] a = {2,4,1,6,8,5,3,7};
+        int[] result = mergeSortWithOneArray.sort(a);
+        int[] expected = {1,2,3,4,5,6,7,8};
+        assertArrayEquals(expected,result);
+    }
+
+}
