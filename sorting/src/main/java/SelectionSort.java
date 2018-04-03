5734fc3d826e0c43c34caffdff602460a78c8e6f
Vineeth V Nair <vineethvnair0@gmail.com>
2018-04-03 22:03:17 +0530

    modified selection sort swap

--- sorting/src/main/java/SelectionSort.java
+++ sorting/src/main/java/SelectionSort.java
@@ -12,9 +12,11 @@ public class SelectionSort {
             for (int j=i+1;j<arr.length;j++)
                 if (arr[j] < arr[imin])
                     imin = j;
-            int temp = arr[i];
-            arr[i] = arr[imin];
-            arr[imin] = temp;
+            if(imin != i) {
+                int temp = arr[i];
+                arr[i] = arr[imin];
+                arr[imin] = temp;
+            }
         }
     }
 }
